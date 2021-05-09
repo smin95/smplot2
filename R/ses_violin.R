1741012d9ee576a0c3ea8969a5ce4754fbd46c89
smin95 <seung.min@mail.mcgill.ca>
2021-05-09 17:39:05 -0400

    aa

--- R/ses_violin.R
+++ R/ses_violin.R
@@ -10,8 +10,6 @@
 #' @param points
 #' Logical argument is required. TRUE if points need to be displayed.
 #' FALSE if points need to be not shown.
-#' @param point_color
-#' Color of the points.
 #' @param point_size
 #' Size of the points.
 
@@ -26,44 +24,70 @@
 #'
 #' @examples
 #'
-ses_violin <- function(point_color,violin_fill_color = 'gray90',
+ses_violin <- function(point_fill,
+                       violin_fill_color,
                        violin_width_color = 'transparent',
                        points = TRUE,
                        point_size = 1,
                        point_width_color = 'transparent',
-                       point_fill = 'black', point_shape = 21) {
+                       point_shape = 21) {
 
-  if (point_shape %in% seq(from=0, to=20)) {
-    if (points == TRUE) {
-      list(ggplot2::geom_violin(fill = violin_fill_color,
-                           color = violin_width_color),
-        ggplot2::geom_point(position = position_jitter(width = .12,
-                                                       height = 0,
-                                                       seed = 10),
-                            size = point_size,
-                            color = point_fill,
-                            shape = point_shape))
-    } else if (points == FALSE) {
-      ggplot2::geom_violin(fill = violin_fill_color,
-                           color = violin_width_color)
+  if (missing(point_fill) && missing(violin_fill_color)) {
+    if (point_shape %in% seq(from=0, to=20)) {
+      if (points == TRUE) {
+        list(ggplot2::geom_violin(fill = violin_fill_color,
+                                  color = violin_width_color),
+             ggplot2::geom_point(position = position_jitter(width = .12,
+                                                            height = 0,
+                                                            seed = 10),
+                                 size = point_size,
+                                 color = point_fill,
+                                 shape = point_shape))
+      } else if (points == FALSE) {
+        ggplot2::geom_violin(fill = violin_fill_color,
+                             color = violin_width_color)
+      }
+    } else if (point_shape %in% c(21,22,23,24,25)) {
+      if (points == TRUE) {
+        list(ggplot2::geom_violin(fill = violin_fill_color,
+                                  color = violin_width_color),
+             ggplot2::geom_point(position = position_jitter(width = .12,
+                                                            height = 0,
+                                                            seed = 10),
+                                 size = point_size,
+                                 fill = point_fill,
+                                 color = point_width_color,
+                                 shape = point_shape))
+      } else if(points == FALSE) {
+        ggplot2::geom_violin(fill = violin_fill_color,
+                             color = violin_width_color)
+      }
+    } else {
+      stop('Such a shape does not exist in geom_point().')
     }
-  } else if (point_shape %in% c(21,22,23,24,25)) {
-    if (points == TRUE) {
-      list(ggplot2::geom_violin(fill = violin_fill_color,
-                           color = violin_width_color),
-        ggplot2::geom_point(position = position_jitter(width = .12,
-                                                       height = 0,
-                                                       seed = 10),
-                            size = point_size,
-                            fill = point_fill,
-                            color = point_width_color,
-                            shape = point_shape))
-    } else if(points == FALSE) {
-      ggplot2::geom_violin(fill = violin_fill_color,
-                           color = violin_width_color)
+  } else if (missing(point_fill) && missing(violin_fill_color)){
+    if (point_shape %in% seq(from=0, to=20)) {
+      if (points == TRUE) {
+        list(ggplot2::geom_violin(),
+             ggplot2::geom_point(position = position_jitter(width = .12,
+                                                            height = 0,
+                                                            seed = 10)))
+      } else if (points == FALSE) {
+        ggplot2::geom_violin()
+      }
+    } else if (point_shape %in% c(21,22,23,24,25)) {
+      if (points == TRUE) {
+        list(ggplot2::geom_violin(),
+             ggplot2::geom_point(position = position_jitter(width = .12,
+                                                            height = 0,
+                                                            seed = 10)))
+      } else if(points == FALSE) {
+        ggplot2::geom_violin()
+      }
+    } else {
+      stop('Such a shape does not exist in geom_point().')
     }
   } else {
-    stop('Such a shape does not exist in geom_point().')
+    stop('both violin_fill_color and point_fill have to exist or be absent.')
   }
-
 }
