13c41604ae823b7b387b198903fe9ce67aad49d2
smin95 <seung.min@mail.mcgill.ca>
2021-05-09 18:22:44 -0400

    a

--- R/ses_boxplot.R
+++ R/ses_boxplot.R
@@ -3,10 +3,10 @@
 #' @description
 #' A violin plot superimposed with jittered individual points.
 #'
-#' @param violin_fill_color
-#' Color of the violin.
-#' @param violin_width_color
-#' Color of the violin's width.
+#' @param boxplot_fill_color
+#' Color of the boxplot.
+#' @param boxplot_width_color
+#' Color of the boxplot's width.
 #' @param points
 #' Logical argument is required. TRUE if points need to be displayed.
 #' FALSE if points need to be not shown.
@@ -24,44 +24,47 @@
 #'
 #' @examples
 #'
-ses_violin <- function(point_fill, violin_fill_color = 'gray90',
-                       violin_width_color = 'transparent',
-                       points = TRUE,
+ses_boxplot <- function(point_fill, boxplot_fill_color = 'gray95',
+                        boxplot_width_color = 'black',
+                       points = TRUE, notch = F, notchwidth = 0.4,
                        point_size = 1,
-                       point_width_color = 'transparent') {
+                       point_width_color = 'white') {
 
   if (missing(point_fill)) {
 
     if (points == TRUE) {
-      list(ggplot2::geom_violin(color = violin_width_color,
-                                fill = violin_fill_color),
+      list(ggplot2::geom_boxplot(color = boxplot_width_color,
+                                fill = boxplot_fill_color,
+                                notch = notch, notchwidth = notchwidth),
            ggplot2::geom_point(position = position_jitter(width = .12,
                                                           height = 0,
                                                           seed = 10)))
     } else if (points == FALSE) {
-      ggplot2::geom_violin()
+      ggplot2::geom_boxplot(notch = notch, notchwidth = notchwidth)
     }
 
   } else {
     if (points == TRUE) {
-      list(ggplot2::geom_violin(color = violin_width_color,
-                                fill = violin_fill_color),
+      list(ggplot2::geom_boxplot(color = boxplot_width_color,
+                                fill = boxplot_fill_color,
+                                notchwidth = notchwidth),
            ggplot2::geom_point(position = position_jitter(width = .12,
                                                           height = 0,
                                                           seed = 10),
                                color = point_fill))
     } else if (points == FALSE) {
-      ggplot2::geom_violin()
+      ggplot2::geom_boxplot(notch = notch, notchwidth = notchwidth)
     }
     if (points == TRUE) {
-      list(ggplot2::geom_violin(color = violin_width_color,
-                                fill = violin_fill_color),
+      list(ggplot2::geom_boxplot(color = boxplot_width_color,
+                                fill = boxplot_fill_color,
+                                notchwidth = notchwidth),
            ggplot2::geom_point(position = position_jitter(width = .12,
                                                           height = 0,
                                                           seed = 10),
                                color = point_fill))
     } else if(points == FALSE) {
-      ggplot2::geom_violin()
+      ggplot2::geom_boxplot(notch = notch, notchwidth = notchwidth)
     }
     else {
       stop('Such a shape does not exist in geom_point().')
