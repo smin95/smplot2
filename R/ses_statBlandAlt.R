f46633539a66d6518582c5e6c4bae1beeedaebae
smin95 <seung.min@mail.mcgill.ca>
2021-05-21 20:44:13 -0400

    data output added

--- R/ses_statBlandAlt.R
+++ R/ses_statBlandAlt.R
@@ -8,9 +8,18 @@
 #' standard deviation of the difference,
 #' mean difference, upper and lower limits. These values
 #' are necessary to draw a Bland Altman plot.
+#'
 #' The list returned from this function can be directly
 #' used as an argument for ses_bland_altman(), which
 #' draws a Bland-Altman plot using ggplot2.
+#'
+#' Another output 'data' is a tibble with two columns:
+#' 1) Mean across each pair for each element
+#' (ex. a mean of the 1st element
+#' from the first set and 1st element from the second set), 2)
+#' Difference between each pair for every element.
+#' The output 'data' should be used as a argument for data in ggplot()
+#' when plotting.
 #'
 #' @param first
 #' Data from the first repetition/session
@@ -29,13 +38,14 @@
 
 ses_statBlandAlt <- function(first, second) {
   diff = second - first
-  avg = rowMeans(cbind(first,second),na.rm=T)
+  mean = rowMeans(cbind(first,second),na.rm=T)
   mean_diff = mean(diff)
   sd = sd(diff)
   upper_limit  = mean_diff + 1.96*sd
   lower_limit = mean_diff - 1.96*sd
 
-  res <- list(diff,avg,sd, mean_diff,upper_limit,lower_limit)
+  data <- as_tibble(cbind(mean,diff))
+  res <- list(diff,mean,sd, mean_diff,upper_limit,lower_limit, data)
   names(res) <- c('diff', 'mean', 'sd', 'mean_diff',
                   'upper_limit','lower_limit')
   return(res)
