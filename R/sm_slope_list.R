cfa9ae2d8391671fa2808bc7d080c18582869cd0
smin95 <seung.min@mail.mcgill.ca>
2022-04-08 15:12:25 +0800

    sm_slope_list

--- R/sm_slope_list.R
+++ R/sm_slope_list.R
@@ -1,8 +1,10 @@
-#' Calculating Area under Curve across multiple conditions and subjects
+#' Calculating the slope across multiple conditions and subjects
 #'
 #' @description
-#' This function returns a data frame containing AUCs from a data frame
-#' that contains the original raw data.
+#' This function returns a data frame containing slope (from linear regression)
+#' from a data frame that contains the original raw data.
+#'
+#' The user can use lm() from base R to compute the slope as well.
 
 #' @param data
 #' Name of the variable that stores the data frame that contains
@@ -18,17 +20,19 @@
 
 #' @param x
 #' The name of the column of the data frame that contains the
-#' x-axis points/x coordinates from which the AUC can be calculated.
+#' x-axis points/x coordinates from which the slopes can be calculated.
 #' It must be strings. The column must not have
 #' characters.
 
 #' @param values
 #' The name of the column of the data frame that contains the
 #' actual data, which are the y-axis points from which the
-#' AUC can be calculated. It must be strings.
+#' slope can be calculated. It must be strings.
 #'
 #' @export
+#' @importFrom stats coef lm
 #' @examples
+#'
 #' \dontrun{
 #' set.seed(1) # generate random data
 #' day1 = rnorm(16,0,1)
@@ -39,36 +43,40 @@
 #' Condition <- rep('Control', length(day1)*2)
 #' df <- cbind(Subject, Value, Condition, Day)
 #'
-#' sm_auc_list(data = df, subjects = 'Subject',values = 'Value',
+#' sm_slope_list(data = df, subjects = 'Subject',values = 'Value',
 #' conditions = 'Condition',x = 'Day')
 #'
 #' }
 #'
-sm_auc_list <- function(data, subjects, conditions, x, values) {
+sm_slope_list <- function(data, subjects, conditions, x, values) {
   data[[conditions]] <- as.factor(data[[conditions]])
-  x_val <- unique(data[[x]])
+
   subjects_list <- unique(base::as.character(data[[subjects]]))
   subj_num <- length(subjects_list)
   cond_list <- unique(data[[conditions]])
   cond_num <- length(cond_list)
+  x_val <- unique(data[[x]])
   x_length <- length(unique(x_val))
 
-  auc_list <- data.frame(matrix(ncol = 3, nrow = subj_num*cond_num))
-  names(auc_list) <- c(subjects, conditions, paste0('AUC_', values))
+  slope_list <- data.frame(matrix(ncol = 3, nrow = subj_num*cond_num))
+  names(slope_list) <- c(subjects, conditions, paste0('Slope'))
 
   for (iCond in seq_along(1:cond_num)) {
     for (iSubj in seq_along(1:subj_num)) {
       ind <- which(data[[conditions]] == unique(cond_list)[iCond] &
                      data[[subjects]] == unique(subjects_list)[iSubj])
+      lin_model <- lm(data[[values]][ind] ~ x_val)
 
-      auc_list[,1][(cond_num*(iSubj-1))+(iCond)] <- subjects_list[iSubj]
-      auc_list[,2][(cond_num*(iSubj-1))+(iCond)] <- cond_list[iCond]
-      auc_list[,3][(cond_num*(iSubj-1))+(iCond)] <- sm_auc(x_val,data[[values]][ind])
+      slope_list[,1][(cond_num*(iSubj-1))+(iCond)] <- subjects_list[iSubj]
+      slope_list[,2][(cond_num*(iSubj-1))+(iCond)] <- cond_list[iCond]
+      slope_list[,3][(cond_num*(iSubj-1))+(iCond)] <- coef(lin_model)[[2]]
     }
   }
-  auc_list[[conditions]] <- base::as.factor(auc_list[[conditions]])
-  levels(auc_list[[conditions]]) <- levels(data[[conditions]])
+  slope_list[[conditions]] <- base::as.factor(slope_list[[conditions]])
+  levels(slope_list[[conditions]]) <- levels(data[[conditions]])
 
-  print(paste('AUC =', values, '*', x))
-  return(auc_list)
+  print(paste0('Slope = ', values, ' ~ ', x))
+
+  return(slope_list)
+
 }
