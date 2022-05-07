7002a636c36f5f8f81ac52fea611127315c332a8
smin95 <seung.min@mail.mcgill.ca>
2022-05-07 20:43:23 +0800

    sm_auc_list and sm_slope_list additional arguments of 'conditions' and 'groups'

--- R/sketch2.R
+++ R/sketch2.R
@@ -1,57 +1,3 @@
-#' Calculating the slope across multiple conditions and subjects
-#'
-#' @description
-#' This function returns a data frame containing slope (from linear regression)
-#' from a data frame that contains the original raw data.
-#'
-#' The user can use lm() from base R to compute the slope as well.
-
-#' @param data
-#' Name of the variable that stores the data frame that contains
-#' the columns with the specified column names.
-#'
-#' @param subjects
-#' The name of the column of the data frame that contains subjects.
-#' It must be strings.
-#'
-#' @param groups
-#' The name of the column of the data frame that contains each guorp.
-#' It must be strings.
-
-#' @param conditions
-#' The name of the column of the data frame that contains each condition.
-#' It must be strings.
-
-#' @param x
-#' The name of the column of the data frame that contains the
-#' x-axis points/x coordinates from which the slopes can be calculated.
-#' It must be strings. The column must not have
-#' characters.
-
-#' @param values
-#' The name of the column of the data frame that contains the
-#' actual data, which are the y-axis points from which the
-#' slope can be calculated. It must be strings.
-#'
-#' @export
-#' @importFrom stats coef lm
-#' @examples
-#'
-#' \dontrun{
-#' set.seed(1) # generate random data
-#' day1 = rnorm(16,0,1)
-#' day2 = rnorm(16,5,1)
-#' Subject <- rep(paste0('S',seq(1:16)), 2)
-#' Value <- data.frame(Value = matrix(c(day1,day2),ncol=1))
-#' Day <- rep(c(1,2), each = length(day1))
-#' Condition <- rep('Control', length(day1)*2)
-#' df <- cbind(Subject, Value, Condition, Day)
-#'
-#' sm_slope_list(data = df, subjects = 'Subject',values = 'Value',
-#' conditions = 'Condition',x = 'Day')
-#'
-#' }
-#'
 sm_slope_list <- function(data, subjects, groups, conditions, x, values) {
 
   x_val <- unique(as.numeric(base::as.character(data[[x]])))
@@ -152,3 +98,5 @@ sm_slope_list <- function(data, subjects, groups, conditions, x, values) {
   print(paste('Slope = ', values, ' ~ ', x))
   return(slope_list)
 }
+
+
--- R/sm_auc_list.R
+++ R/sm_auc_list.R
@@ -1,8 +1,11 @@
 #' Calculating Area under Curve across multiple conditions and subjects
 #'
+#'
 #' @description
 #' This function returns a data frame containing AUCs from a data frame
-#' that contains the original raw data.
+#' that contains the original raw data. One of the two arguments 'groups'
+#' and 'conditions' must be filled. The function will throw an error
+#' if both arguments are empty.
 
 #' @param data
 #' Name of the variable that stores the data frame that contains
@@ -10,6 +13,10 @@
 #'
 #' @param subjects
 #' The name of the column of the data frame that contains subjects.
+#' It must be strings.
+#'
+#' @param groups
+#' The name of the column of the data frame that contains each guorp.
 #' It must be strings.
 
 #' @param conditions
@@ -44,31 +51,96 @@
 #'
 #' }
 #'
-sm_auc_list <- function(data, subjects, conditions, x, values) {
-  data[[conditions]] <- as.factor(data[[conditions]])
+sm_auc_list <- function(data, subjects, groups, conditions, x, values) {
+
   x_val <- unique(data[[x]])
   subjects_list <- unique(base::as.character(data[[subjects]]))
   subj_num <- length(subjects_list)
-  cond_list <- unique(data[[conditions]])
-  cond_num <- length(cond_list)
   x_length <- length(unique(x_val))
 
-  auc_list <- data.frame(matrix(ncol = 3, nrow = subj_num*cond_num))
-  names(auc_list) <- c(subjects, conditions, paste0('AUC_', values))
+  if (missing(groups) && missing(conditions)) {
+    stop('conditions or groups argument has to be filled')
+  } else if (missing(groups)) {
+    cond_list <- unique(data[[conditions]])
+    cond_num <- length(cond_list)
+    data[[conditions]] <- as.factor(data[[conditions]])
 
-  for (iCond in seq_along(1:cond_num)) {
-    for (iSubj in seq_along(1:subj_num)) {
-      ind <- which(data[[conditions]] == unique(cond_list)[iCond] &
-                     data[[subjects]] == unique(subjects_list)[iSubj])
+    auc_list <- data.frame(matrix(ncol = 3, nrow = subj_num*cond_num))
+    names(auc_list) <- c(subjects, conditions, paste0('AUC_', values))
 
-      auc_list[,1][(cond_num*(iSubj-1))+(iCond)] <- subjects_list[iSubj]
-      auc_list[,2][(cond_num*(iSubj-1))+(iCond)] <- cond_list[iCond]
-      auc_list[,3][(cond_num*(iSubj-1))+(iCond)] <- sm_auc(x_val,data[[values]][ind])
+    for (iCond in seq_along(1:cond_num)) {
+      for (iSubj in seq_along(1:subj_num)) {
+        ind <- which(data[[conditions]] == unique(cond_list)[iCond] &
+                       data[[subjects]] == unique(subjects_list)[iSubj])
+        print(ind)
+        auc_list[,1][(cond_num*(iSubj-1))+(iCond)] <- subjects_list[iSubj]
+        auc_list[,2][(cond_num*(iSubj-1))+(iCond)] <- cond_list[iCond]
+        auc_list[,3][(cond_num*(iSubj-1))+(iCond)] <- sm_auc(x_val,data[[values]][ind])
+      }
     }
-  }
-  auc_list[[conditions]] <- base::as.factor(auc_list[[conditions]])
-  levels(auc_list[[conditions]]) <- levels(data[[conditions]])
+    auc_list[[conditions]] <- base::as.factor(auc_list[[conditions]])
+    levels(auc_list[[conditions]]) <- levels(data[[conditions]])
 
+  } else if (missing(conditions)) {
+    group_list <- unique(data[[groups]])
+    group_num <- length(group_list)
+    data[[groups]] <- as.factor(data[[groups]])
+
+
+    auc_list <- data.frame(matrix(ncol = 3, nrow = subj_num*group_num))
+    names(auc_list) <- c(subjects, groups, paste0('AUC_', values))
+
+    for (iGroup in seq_along(1:group_num)) {
+      for (iSubj in seq_along(1:subj_num)) {
+        ind <- which(data[[groups]] == unique(group_list)[iGroup] &
+                       data[[subjects]] == unique(subjects_list)[iSubj])
+
+        auc_list[,1][(group_num*(iSubj-1))+(iGroup)] <- subjects_list[iSubj]
+        auc_list[,2][(group_num*(iSubj-1))+(iGroup)] <- group_list[iGroup]
+        auc_list[,3][(group_num*(iSubj-1))+(iGroup)] <- sm_auc(x_val,data[[values]][ind])
+      }
+    }
+    auc_list[[groups]] <- base::as.factor(auc_list[[groups]])
+    levels(auc_list[[groups]]) <- levels(data[[groups]])
+
+  } else {
+
+    cond_list <- unique(data[[conditions]])
+    cond_num <- length(cond_list)
+    data[[conditions]] <- as.factor(data[[conditions]])
+
+    group_list <- unique(data[[groups]])
+    group_num <- length(group_list)
+    data[[groups]] <- as.factor(data[[groups]])
+
+    auc_list <- data.frame(matrix(ncol = 4, nrow = subj_num*cond_num*group_num))
+
+    names(auc_list) <- c(subjects, conditions, groups, paste0('AUC_', values))
+
+    for (iGroup in seq_along(1:group_num)) {
+      for (iCond in seq_along(1:cond_num)) {
+        for (iSubj in seq_along(1:subj_num)) {
+          ind <- which(data[[groups]] == unique(group_list)[iGroup] &
+                         data[[conditions]] == unique(cond_list)[iCond] &
+                         data[[subjects]] == unique(subjects_list)[iSubj])
+          if (length(ind)!=0) {
+            auc_list[,1][(group_num*cond_num*(iSubj-1))+(iGroup+iCond)] <- subjects_list[iSubj]
+            auc_list[,2][(group_num*cond_num*(iSubj-1))+(iGroup+iCond)] <- cond_list[iCond]
+            auc_list[,3][(group_num*cond_num*(iSubj-1))+(iGroup+iCond)] <- group_list[iGroup]
+            auc_list[,4][(group_num*cond_num*(iSubj-1))+(iGroup+iCond)] <- sm_auc(x_val,data[[values]][ind])
+          }
+
+        }
+      }
+    }
+
+    auc_list[[groups]] <- base::as.factor(auc_list[[groups]])
+    levels(auc_list[[groups]]) <- levels(data[[groups]])
+    auc_list[[conditions]] <- base::as.factor(auc_list[[conditions]])
+    levels(auc_list[[conditions]]) <- levels(data[[conditions]])
+
+  }
+  auc_list <- na.omit(auc_list)
   print(paste('AUC =', values, '*', x))
   return(auc_list)
 }
